5fd17a09b99e4c79ff4a02233d5c341bd481c68e
Quintin <quinndonnelly22@gmail.com>
2016-08-27 15:24:28 -0500

    Added an outline of the jump script needed to cause a jump.

---
+++ Assets/Scripts/jump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+
+	public float jump_velocity;		// This is the amount of velocity that will be multiplied to cause the jump
+
+	void Update()
+	{
+		float x_diff = 0;
+		float z_diff = 0;
+	
+		Vector3 force = new Vector3 (x_diff, 0.0f, z_diff);
+		force.Normalize();
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce(force * jump_velocity, ForceMode.Acceleration);
+	}
+}
